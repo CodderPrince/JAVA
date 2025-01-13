674ec51fd76cbd73611d4ff543caaad6a15385a0
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:02:18 +0600

    Create e.java

---
+++ JAVA/Project 1/Project1/FirstProject/src/e.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class e {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        System.out.print("Enter the value of x: ");
+        x = sc.nextInt();
+
+        System.out.println("Value of x: " + x);
+
+    }
+
+}
