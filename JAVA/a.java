3ae79063071edfc35bd7e6ba76b24f8ad7b2fa73
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:01:57 +0600

    Create a.java

---
+++ JAVA/a.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class a {
+    public static void main(String[] args) {
+        int x=5;
+        switch(x)
+        {
+            case 1:
+            System.out.println("Bangladesh");
+            break;
+
+            case 2:
+            System.out.println("Rangpur");
+            break;
+
+            default:
+            System.out.println("No one true");
+        }
+    }
+}
