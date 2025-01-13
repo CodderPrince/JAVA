78fd3852a9dcdde50df151a45088b01804fd3654
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:03:52 +0600

    Create MainActivity.java

---
+++ pTUbe/app/src/main/java/com/cscorner/ptube/MainActivity.java
@@ -0,0 +1,36 @@
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        // Enable JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Load YouTube URL
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com");
+    }
+
+    // Handle back button navigation
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
