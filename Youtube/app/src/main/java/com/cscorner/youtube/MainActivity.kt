ffede8ad8ee44a75845ab1868aab3b2a856f8dd9
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:04:21 +0600

    Create MainActivity.kt

---
+++ Youtube/app/src/main/java/com/cscorner/youtube/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.cscorner.youtube
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Ensure activity_main.xml exists
+
+        webView = findViewById(R.id.webView)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.youtube.com")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
