765eb2d77d171e50fb5dcc2c6b549a37e26a6dce
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:01:34 +0600

    Create JournalManager.java

---
+++ Java Project 1/journal/JournalManager.java
@@ -0,0 +1,10 @@
+package journal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JournalManager {
+    private List<Journal> journals = new ArrayList<>();
+
+    // Methods to manage journals (add, remove, display, etc.)
+}
