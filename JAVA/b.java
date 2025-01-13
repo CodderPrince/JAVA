57b68ca63b724b5f1ff678afc557e8ff0cbfd938
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:01:58 +0600

    Create b.java

---
+++ JAVA/b.java
@@ -0,0 +1,15 @@
+public class b {
+    public static void main(String[] args) {
+        /**text */
+        /**Dhaka;
+        Bangladesh*/
+        System.out.print("Hello Prince");
+        /**Hello bd
+         * How are you
+         * public
+         * @static
+         * 
+         */
+    }
+    
+}
