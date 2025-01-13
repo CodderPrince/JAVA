490471f1079154793716349489dc79c110f2c462
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:01:31 +0600

    Create Book.java

---
+++ Java Project 1/books/Book.java
@@ -0,0 +1,11 @@
+package books;
+
+public class Book {
+    private String bookId;
+    private String accessionNumber;
+    private String bookName;
+    private String author;
+    private String publication;
+
+    // Constructor, getters, and setters
+}
