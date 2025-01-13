cb64c0bffdb22732ff7128ff64642d3ca678cb43
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:02:20 +0600

    Create f.java

---
+++ JAVA/Project 1/Project1/FirstProject/src/f.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class f {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s =sc.nextLine();
+        System.out.println(s);
+        int x;
+        //x = sc.nextInt();
+        //System.out.println(x);
+    }
+    
+}
