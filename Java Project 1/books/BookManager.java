4cf20919dcf1b0b7524e9c416be9f0fe6feef4e5
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:01:33 +0600

    Create BookManager.java

---
+++ Java Project 1/books/BookManager.java
@@ -0,0 +1,10 @@
+package books;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+    private List<Book> books = new ArrayList<>();
+
+    // Methods to manage books (add, remove, display, etc.)
+}
