4db80f07da97e7268054f649ba770283a0367f79
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-14 01:02:15 +0600

    Create c.java

---
+++ JAVA/Project 1/Project1/FirstProject/src/c.java
@@ -0,0 +1,17 @@
+public class c {
+    public static void main(String[] args) {
+        String st = "Hi, I'm good";
+        String s2 = new String("Bangladesh");
+        System.out.println(st + " " + s2);
+        System.out.println(st.length());
+        System.out.println(st.toUpperCase());
+        System.out.println(st.toLowerCase());
+        System.out.println(st.charAt(4));
+
+        if(st.equals(s2))
+        System.out.println("They are equal");
+        else
+        System.out.println("Not equal");
+    }
+
+}
